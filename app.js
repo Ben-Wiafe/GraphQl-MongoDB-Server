271dea710c3434f8b90c86aed6fc7916fcdb063b
Azay <azay.genius8@gmail.com>
2021-10-29 15:37:14 +0100

    Relocate schema object

--- app.js
+++ app.js
@@ -1,69 +1,41 @@
 const { ApolloServer, gql } = require("apollo-server");
 const schema = require("./schema/schema");
-const authors = require("./schema/Author Model.json");
-const books = require("./schema/Book Model.json");
-const lodash = require("lodash");
-
-const port = 3000;
- /////////////////////////
-
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 var mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://azaygenius:Password@blog.prlnz.mongodb.net/blogDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://azaygenius:Password@blog.prlnz.mongodb.net/Blogdb?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected..");
   })
   .catch((error) => console.log(error));
 
-var BlogSchema = new mongoose.Schema({
-  id: {
-    type:  String
-  },
-  comments: {
-    type: String,
-  },
-  likes: {
-    type: Number,
-  },
-  text: {
-    type: String,
-  },
-  banner: {
-    type: String
-  },
-  authorID: {
-    type: String,
-  },
-  createdAt: {
-    type: String,
-  },
-});
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
-module.exports = new mongoose.model("Blog", BlogSchema);
+db.once("open", function () {
+  console.log("Connection Successful!");
 
-const app = express();
+  const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+  app.get("/", (req, res) => {
+    res.send("Home Page");
+  });
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  );
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  const port = 3000;
 
-app.listen(port, () => {
-  console.log("Server running on port 3000");
+  app.listen(port, () => {
+    console.log("Server running on port 3000");
+  });
 });
