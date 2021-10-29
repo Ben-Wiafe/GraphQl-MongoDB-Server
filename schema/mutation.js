c65dc7660484160ad96eada8663a2416aed3f9da
Azay <azay.genius8@gmail.com>
2021-10-29 15:39:31 +0100

    Complete create blog mutation

--- schema/mutation.js
+++ schema/mutation.js
@@ -1,32 +1,17 @@
 const graphql = require("graphql");
-<<<<<<< HEAD
-const BookType = require("./type_defs/book_type");
-=======
 const PostType = require("./type_defs/post_type");
-const CommentType = require("./type_defs/comment_type");
 const uuid = require("uuid");
-const posts = require("../posts");
->>>>>>> a469b9c5252b7a53d088f8bada3771a5cfd652d6
+const Blog = require("./blog_model");
 
 const Mutation = new graphql.GraphQLObjectType({
   name: "Mutation",
   fields: {
-<<<<<<< HEAD
-    add_book: {   
-      type: BookType,
-      args: { 
-        title: { type: graphql.GraphQLString },
-        genre: { type: graphql.GraphQLString },
-        isbn: { type: graphql.GraphQLString },
-        pages: { type: graphql.GraphQLString },
-=======
     create_blog: {
       type: PostType,
       args: {
         text: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
         banner: { type: graphql.GraphQLString },
         authorID: { type: graphql.GraphQLString },
->>>>>>> a469b9c5252b7a53d088f8bada3771a5cfd652d6
       },
       resolve(parent, args) {
         var postID = uuid.v4();
@@ -34,13 +19,14 @@ const Mutation = new graphql.GraphQLObjectType({
         let data = args;
 
         data.createdAt = today;
-        data.id = postID;
+        data._id = postID;
         data.likes = 0;
         data.comments = 0;
-
-        posts.push(data);
-
-        // Write code here to insert the "data" object into database
+        
+        blog.save(function (err, blogg) {
+          if (err) return console.error(err);
+          console.log("Saved to blogs collection");
+        });
       },
     },
   },
